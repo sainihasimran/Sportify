744a113d95cc983fa24e7531ce7200dd81ed5516
Harsimran Saini <harsimran12326@gmail.com>
2021-06-29 02:19:21 -0400

    Setup product details activity

---
+++ app/src/main/java/com/cegep/sportify/productdetails/ProductDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.cegep.sportify.productdetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cegep.sportify.R;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
