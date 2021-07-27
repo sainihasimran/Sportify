53b9336929b7700326700df497b93b8d199088c4
Hemit Khunt <khunthemit1@gmail.com>
2021-07-27 19:19:39 -0400

    Setup search adapter

---
+++ app/src/main/java/com/cegep/sportify/search/Adapter/SearchItemAdapter.java
@@ -0,0 +1,27 @@
+package com.cegep.sportify.search.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SearchItemAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/cegep/sportify/search/SearchFragment.java
+++ app/src/main/java/com/cegep/sportify/search/SearchFragment.java
@@ -4,20 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cegep.sportify.R;
+import com.cegep.sportify.Utils;
+import com.cegep.sportify.search.Adapter.SearchItemAdapter;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class SearchFragment extends Fragment {
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
+    private SearchItemAdapter searchItemAdapter;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        emptyView = view.findViewById(R.id.empty_view);
+        setupRecyclerView(view);
+    }
+
+    private void setupRecyclerView(View view) {
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 2));
+        recyclerView.setAdapter(searchItemAdapter);
+
     }
 }
